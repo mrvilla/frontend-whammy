4a0b6934ff971623e99a0ff2522b2fdad5ec6892
iammrvilla <iammrvilla@gmail.com>
2024-09-16 13:59:14 +0200

    add: tailwind cong and lint config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        airbnb:"#FF385C",
-        airbnb_dark:"#D50027",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        airbnb: '#FF385C',
+        airbnb_dark: '#D50027',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
